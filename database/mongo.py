f78fa8fa93d066a6106300c83e0a68b7f2786dee
Alan B <death1027@outlook.com>
2023-09-12 12:59:57 -0600

    fix: :bug: fix ssl cert in mongo connect

--- database/mongo.py
+++ database/mongo.py
@@ -1,7 +1,9 @@
 import motor.motor_asyncio
 import os
+import certifi
+
 
 MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB = os.getenv("MONGO_DB")
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI, tlsCAFile=certifi.where())
 database = client[MONGO_DB]
